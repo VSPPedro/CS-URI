d2eaef0ef7efac3857c825b9a03bf201a7d9360e
Pedro Paiva <pedrovsp109@gmail.com>
2016-10-28 20:58:35 -0300

    2147 - Galopeira

---
+++ Program2147.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWords;
+            string word;
+
+            //To print "." not "," in numeric values
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            numberOfWords = Convert.ToInt16(Console.ReadLine());
+
+            for(int i = 0; i < numberOfWords; i++)
+            {
+                word = Console.ReadLine();
+                Console.WriteLine("{0:N2}", word.Length * 0.01);
+            }
+        }
+    }
+}
