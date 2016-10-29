03916d33686307265ab570834fd654a67c22b48d
Pedro Paiva <pedrovsp109@gmail.com>
2016-10-29 19:53:59 -0300

    Add 2160 - Nome no Formulário

---
+++ Program2160.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2160
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine((text.Length > 80) ? "NO" : "YES");
+        }
+    }
+}
