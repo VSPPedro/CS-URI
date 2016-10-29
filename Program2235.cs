d42c021c45914782c701d2385996050b3eeb0189
Pedro Paiva <pedrovsp109@gmail.com>
2016-10-29 08:09:17 -0300

    2235 - Andando no Tempo

---
+++ Program2235.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbera, numberb, numberc;
+
+            string input = Console.ReadLine();
+
+            string[] numbers = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            numbera = Convert.ToInt16(numbers[0]);
+
+            numberb = Convert.ToInt16(numbers[1]);
+
+            numberc = Convert.ToInt16(numbers[2]);
+
+            if (numbera == numberb || numbera == numberc || numberb == numberc || (numbera + numberb) == numberc 
+                || (numbera + numberc) == numberb || (numberb + numberc) == numbera)
+            {
+                Console.WriteLine("S");
+            }
+            else
+            {
+                Console.WriteLine("N");
+            }
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
