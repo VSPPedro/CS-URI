faa4f452039b01d920838e9ff38646df669ce6db
Pedro Paiva <pedrovsp109@gmail.com>
2016-10-30 10:47:44 -0300

    Add 2242 - HuahauHAUhauhauhauHAU

---
+++ Program2242.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2242
+    {
+        static void Main(string[] args)
+        {
+            string risada = Console.ReadLine();
+
+            string vogais = "AaEeIiOoUu";
+
+            string normal = "";
+            string inverso = "";
+
+            for(int x = 0; x < risada.Length; x++)
+            {
+                for(int y = 0; y < vogais.Length; y++)
+                {
+                    if (risada[x] == vogais[y])
+                    {
+                        normal += risada[x];
+                        inverso = risada[x] + inverso;
+                    }
+                }
+            }
+
+            if(normal == inverso)
+            {
+                Console.WriteLine("S");
+            }
+            else
+            {
+                Console.WriteLine("N");
+            }
+
+        }
+
+    }
+}
