c800b1d505bff99d5ddb4376fe4a11d42b08c5b4
Pedro Paiva <pedrovsp109@gmail.com>
2016-10-30 12:35:53 -0300

    Add 2164 - Fibonacci Rápido

---
+++ Program2164.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2164
+    {
+        static void Main(string[] args)
+        {
+            int entrada = Convert.ToInt16(Console.ReadLine());
+          
+            double fibonnaci = (Math.Pow((1 + Math.Sqrt(5)) / 2, entrada) - Math.Pow((1 - Math.Sqrt(5)) / 2, entrada)) / Math.Sqrt(5);
+
+            Console.WriteLine((fibonnaci).ToString("0.0").Replace(",","."));
+        }
+    }
+}
