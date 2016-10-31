80e1dcd2d63b3e1bebb272b42b3b33b9b8d99a38
Pedro Paiva <pedrovsp109@gmail.com>
2016-10-30 23:15:51 -0300

    Add 2172 - Evento

---
+++ Program2172.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2172
+    {
+        static void Main(string[] args)
+        {
+            Program2172 uri = new Program2172();
+
+            string numbers = Console.ReadLine();
+
+            Int64 aumento = 0;
+            Int64 xp = 0;
+
+            uri.converterParaNumeros(numbers, ref aumento, ref xp);
+
+            while(aumento != 0 || xp != 0)
+            {
+                Console.WriteLine(aumento * xp);
+
+                numbers = Console.ReadLine();
+                uri.converterParaNumeros(numbers, ref aumento, ref xp);
+            }
+        }
+
+        void converterParaNumeros(string input, ref Int64 aumento, ref Int64 xp)
+        {
+            string[] array_numbers = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            aumento = Convert.ToInt64(array_numbers[0]);
+            xp = Convert.ToInt64(array_numbers[1]);
+        }
+    }
+}
