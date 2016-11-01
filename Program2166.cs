8ae81b8176855c94268fc6e46848dcc7b8c1d255
Pedro Paiva <pedrovsp109@gmail.com>
2016-11-01 20:56:49 -0300

    Add 2166 - Raiz de 2

---
+++ Program2166.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2166
+    {
+        static void Main(string[] args)
+        {
+            double temp = 2;
+            int repeticoes = Convert.ToInt16(Console.ReadLine());
+
+            if (repeticoes == 0)
+            {
+                Console.WriteLine("1.0000000000");
+            }
+            else
+            {
+                for (int i = 1; i < repeticoes; i++)
+                {
+                    temp = 2 + 1 / temp;
+                }
+                temp = 1 + 1 / temp;
+
+                string resultado = temp.ToString("0.0000000000").Replace(",", ".");
+
+                Console.WriteLine(resultado);
+            }
+        }
+    }
+}
