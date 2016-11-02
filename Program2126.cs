a84385a6916e6c40cf7a52d00128d1f0df411064
Pedro Paiva <pedrovsp109@gmail.com>
2016-11-02 11:39:01 -0300

    Add 2126 - Procurando Subsequencias

---
+++ Program2126.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Uri_Awnsers
+{
+    class Program2126
+    {
+        
+        static void Main(string[] args)
+        {
+            int caso = 1;
+            string repeticao = Console.ReadLine();
+
+            while(repeticao != null && repeticao != "")
+            {
+                string msg = Console.ReadLine();
+                
+                int quantSubSequencias = 0;
+                int positionAnt = 0;
+                int position = msg.IndexOf(repeticao);
+                
+                while (position != -1)
+                {
+                    quantSubSequencias++;
+                    int startindex = position + repeticao.Length;
+                    positionAnt = position + 1;
+                    position = msg.IndexOf(repeticao, startindex);
+
+                }
+                
+                if (quantSubSequencias != 0)
+                {
+                    Console.WriteLine("Caso #" + caso + ":");
+                    Console.WriteLine("Qtd.Subsequencias: " + quantSubSequencias);
+                    Console.WriteLine("Pos: " + positionAnt);
+                }
+                else
+                {
+                    Console.WriteLine("Caso #" + caso + ":");
+                    Console.WriteLine("Nao existe subsequencia");
+                }
+
+                Console.WriteLine();
+
+                repeticao = Console.ReadLine();
+                caso++;
+            }
+        }
+    }
+}
